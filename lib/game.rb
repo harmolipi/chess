7a33fc5306d66b60eab63c44cba865b4b7499a40
harmolipi <nikob381@gmail.com>
2021-05-07 08:51:19 -0400

    contents for testing

--- lib/game.rb
+++ lib/game.rb
@@ -3,8 +3,10 @@
 require_relative './board'
 require_relative './pawn'
 
-my_pawn = Pawn.new('black')
+my_pawn = Pawn.new('black', [0, 1])
 # p my_pawn.location
 # puts my_pawn.symbol
 my_board = Board.new
 my_board.to_s
+puts "\n"
+my_board.display_possible_moves(my_pawn)
