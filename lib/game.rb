14129665d13fadf30f39c9b7a52b425c492fcb68
harmolipi <nikob381@gmail.com>
2021-06-13 16:36:37 -0400

    Started moving input methods from board

--- lib/game.rb
+++ lib/game.rb
@@ -7,12 +7,13 @@ require 'pry'
 class Game
   attr_reader :current_player
 
-  GAME_TYPE_TEST = /^(1|2)$/
+  GAME_TYPE_TEST = /^(1|2)$/.freeze
+  VALID_COORDINATES = /^([A-H]|[a-h])[1-8]$/.freeze
 
   def initialize
     @current_player = 'white'
     @chess_board = Board.new
-    game_over? = false
+    @game_over = false
   end
 
   def intro_text
@@ -42,7 +43,31 @@ class Game
   end
 
   def two_player_game_loop
+    puts "\n"
+    @chess_board.to_s
+    puts "\n"
+    # print "Enter the coordinates (e.g. 'a1') for the piece you'd like to move: "
+    chosen_piece = coordinates_input
+    p chosen_piece
+  end
 
+  def coordinates_input
+    loop do
+      print "Enter coordinates (e.g. 'a2') for the piece you want to move: "
+      coordinates = gets.chomp
+      return map_coordinates(coordinates) if valid_coordinates?(coordinates)
+
+      puts 'Invalid entry, please try again.'.red
+    end
+  end
+
+  def map_coordinates(coordinates)
+    # converts chess coordinates to array coordinates
+    [coordinates[0].downcase.codepoints[0] - 97, coordinates[1].to_i - 1]
+  end
+
+  def valid_coordinates?(coordinates)
+    coordinates.match(VALID_COORDINATES)
   end
 
   def player_turn
