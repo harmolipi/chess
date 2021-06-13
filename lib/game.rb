c11f184839f0c9277e8195dbe14f59ad1d39be19
harmolipi <nikob381@gmail.com>
2021-06-13 15:50:13 -0400

    Created #game_type_input

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,52 @@
 # frozen_string_literal: true
 
+require_relative './board'
+require 'pry'
+
+# Class for chess game rules
 class Game
+  attr_reader :current_player
+
+  GAME_TYPE_TEST = /^(1|2)$/
+
   def initialize
     @current_player = 'white'
+    @chess_board = Board.new
+    game_over? = false
+  end
+
+  def intro_text
+    system 'clear'
+    puts "\nWelcome to Chess!"
+  end
+
+  def game_type
+    # binding.pry
+    game_type = nil
+    while game_type.nil?
+      print "\nPress 1 to play against another human! "
+      game_type = game_type_input
+    end
+    game_type
+  end
+
+  def game_type_input
+    game_type = gets.chomp
+    return game_type if valid_game_type?(game_type)
+
+    puts 'Error: invalid input. Please try again.'.red
+  end
+
+  def valid_game_type?(input)
+    input.match(GAME_TYPE_TEST)
+  end
+
+  def two_player_game_loop
+
+  end
+
+  def player_turn
+
   end
 
   def switch_players
---
+++ lib/main.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative './game'
+require 'pry'
+
+chess_game = Game.new
+
+chess_game.intro_text
+if chess_game.game_type == '1'
+  # chess_game.two_player_game_loop
+  puts '2-player'
+else
+  # chess_game.one_player_game_loop
+  puts '1-player'
+end
+# system 'clear'
