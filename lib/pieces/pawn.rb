e05b05fce6d41cd3c0849e3a0a47c435196b52a2
harmolipi <nikob381@gmail.com>
2021-06-20 16:04:50 -0400

    Updated #possible_moves to new system

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop: disable Metrics/AbcSize
+
 require_relative './piece'
 require 'pry'
 
@@ -13,15 +15,27 @@ class Pawn < Piece
     @movement_direction = @color == 'white' ? 1 : -1
   end
 
-  def move_to
-
-  end
-
   def possible_moves
-    possible_moves = [[@location[0], @location[1] + 1 * @movement_direction]]
-    possible_moves << [@location[0], @location[1] + 2 * @movement_direction] if @location == @original_location
-    possible_moves.select { |move| valid_move?(move) }
+    # binding.pry
+    possible_moves = []
+    possible_moves << [[@location[0], @location[1] + 1 * @movement_direction]]
+    possible_moves[0] << [@location[0], @location[1] + 2 * @movement_direction] if @location == @original_location
+    possible_moves[0].select! { |move| valid_move?(move) }
+    possible_moves
   end
+
+  # def possible_moves
+  #   possible_moves = [[@location[0], @location[1] + 1 * @movement_direction]]
+  #   possible_moves << [@location[0], @location[1] + 2 * @movement_direction] if @location == @original_location
+  #   possible_moves.select! { |move| valid_move?(move) }
+
+  #   possible_moves = []
+  #   current_square = @location
+  #   loop do
+  #     next_square = [current_square[0], current_square[1] + 1 * @movement_direction]
+
+  #   end
+  # end
 
   def possible_attacks
     possible_attacks = []
@@ -30,3 +44,5 @@ class Pawn < Piece
     possible_attacks.select { |move| valid_move?(move) }
   end
 end
+
+# rubocop: enable Metrics/AbcSize
