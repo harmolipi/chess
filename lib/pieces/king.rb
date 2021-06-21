c79b33bba0b7babeaeaf7d7762ee2da12539d554
harmolipi <nikob381@gmail.com>
2021-06-21 11:40:12 -0400

    Added #possible_moves

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -9,4 +9,25 @@ class King < Piece
     black_king = " \u265A "
     super(white_king, black_king, color, location)
   end
+
+  def possible_moves
+    possible_moves = Array.new(8) { [] }
+    location_col = @location[0]
+    location_row = @location[1]
+
+    possible_moves[0] << [location_col, location_row + 1]
+    possible_moves[1] << [location_col + 1, location_row + 1]
+    possible_moves[2] << [location_col + 1, location_row]
+    possible_moves[3] << [location_col + 1, location_row - 1]
+    possible_moves[4] << [location_col, location_row - 1]
+    possible_moves[5] << [location_col - 1, location_row - 1]
+    possible_moves[6] << [location_col - 1, location_row]
+    possible_moves[7] << [location_col - 1, location_row + 1]
+
+    possible_moves.each do |direction|
+      direction.select! { |move| valid_move?(move) }
+    end
+
+    possible_moves
+  end
 end
