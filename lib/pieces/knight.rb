4c29a39247ffd7ed8acaabbff945b790a34523b7
harmolipi <nikob381@gmail.com>
2021-06-21 11:40:28 -0400

    Added #possible_moves

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -9,4 +9,25 @@ class Knight < Piece
     black_knight = " \u265E "
     super(white_knight, black_knight, color, location)
   end
+
+  def possible_moves
+    possible_moves = Array.new(8) { [] }
+    location_col = @location[0]
+    location_row = @location[1]
+
+    possible_moves[0] << [location_col + 1, location_row + 2]
+    possible_moves[1] << [location_col + 2, location_row + 1]
+    possible_moves[2] << [location_col + 2, location_row - 1]
+    possible_moves[3] << [location_col + 1, location_row - 2]
+    possible_moves[4] << [location_col - 1, location_row - 2]
+    possible_moves[5] << [location_col - 2, location_row - 1]
+    possible_moves[6] << [location_col - 2, location_row + 1]
+    possible_moves[7] << [location_col - 1, location_row + 2]
+
+    possible_moves.each do |direction|
+      direction.select! { |move| valid_move?(move) }
+    end
+
+    possible_moves
+  end
 end
