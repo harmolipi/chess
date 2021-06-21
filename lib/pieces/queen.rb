64376d938fc2d94bca394c96420305bb250705ad
harmolipi <nikob381@gmail.com>
2021-06-21 11:40:24 -0400

    Added #possible_moves

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -9,4 +9,48 @@ class Queen < Piece
     black_queen = " \u265B "
     super(white_queen, black_queen, color, location)
   end
+
+  def possible_moves
+    possible_moves = Array.new(8) { [] }
+    location_col = @location[0]
+    location_row = @location[1]
+
+    1.upto(7) do |move|
+      possible_moves[0] << [location_col, location_row + move]
+    end
+
+    1.upto(7) do |move|
+      possible_moves[1] << [location_col + move, location_row + move]
+    end
+
+    1.upto(7) do |move|
+      possible_moves[2] << [location_col + move, location_row]
+    end
+
+    1.upto(7) do |move|
+      possible_moves[3] << [location_col + move, location_row - move]
+    end
+
+    1.upto(7) do |move|
+      possible_moves[4] << [location_col, location_row - move]
+    end
+
+    1.upto(7) do |move|
+      possible_moves[5] << [location_col - move, location_row - move]
+    end
+
+    1.upto(7) do |move|
+      possible_moves[6] << [location_col - move, location_row]
+    end
+
+    1.upto(7) do |move|
+      possible_moves[7] << [location_col - move, location_row + move]
+    end
+
+    possible_moves.each do |direction|
+      direction.select! { |move| valid_move?(move) }
+    end
+
+    possible_moves
+  end
 end
