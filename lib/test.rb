d10a123709003d2841357f2132f42aab20f65dcc
harmolipi <nikob381@gmail.com>
2021-07-02 02:53:37 -0400

    Testing

--- lib/test.rb
+++ lib/test.rb
@@ -9,15 +9,16 @@ my_pawn = Pawn.new('white', [0, 1])
 # p my_pawn.location
 # puts my_pawn.symbol
 my_board = Board.new
-my_board.to_s_colored
+my_board.to_s
 # my_board.print_square(1, my_pawn)
 puts "\n"
 my_board.display_possible_moves(my_pawn)
 # my_board.display_possible_moves(other_pawn)
 puts "\n"
-my_board.to_s_colored
+my_board.to_s
 puts my_board.can_move?(my_pawn, [0, 3])
 # p my_board.coordinates_input
 my_move = my_board.coordinates_input
 my_board.move(my_board.white[:pawn1], my_move)
-my_board.to_s_colored
+my_board.to_s
+my_board.display_selection([3, 1])
